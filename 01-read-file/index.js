6b833084e0cb4899ca272205b8e7f556da7fc601
Maxim Predybailo <predmaxim@ya.ru>
2022-11-06 05:12:15 +0500

    refactor(01-read-file): optimize code

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const fs = require('fs');
+const {stdout} = require('process');
 
 const txt = path.join(__dirname, 'text.txt');
 
-fs.createReadStream(txt).pipe(process.stdout);
+fs.createReadStream(txt,'utf8').pipe(stdout);
 // fs.createReadStream(txt).on('data', chunk => console.log(chunk.toString()));
 
