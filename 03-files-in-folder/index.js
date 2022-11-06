096bc437682f39344987f1d90b9cfe157b8058a3
Maxim Predybailo <predmaxim@ya.ru>
2022-11-06 23:06:54 +0500

    refactor(03-files-in-folder): output view

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,21 +4,32 @@ const fs = require('fs/promises');
 const secretDir = path.join(__dirname, 'secret-folder');
 
 const getFiles = (dirPath) => fs.readdir(dirPath, { withFileTypes: true });
-const getFileSize = (filePath) => fs.stat(filePath, (err, file) => err ? console.log(err) : file.size);
+const getFileSize = (filePath) => fs.stat(filePath, (err, file) => file);
 
 getFiles(secretDir).then(filesStats =>
 
   filesStats.map(el => {
 
-    const fileName = el.name.slice(0, el.name.lastIndexOf('.'));
+    const fileName = el.name.slice(0, el.name.lastIndexOf('.') === -1
+      ? el.name.length
+      : el.name.lastIndexOf('.'));
+
     const filePath = path.join(secretDir, el.name);
     const isFile = el.isFile();
-    const fileExt = path.extname(el.name).slice(1);
+
+    const fileExt = path.extname(el.name)
+      ? path.extname(el.name).slice(1)
+      : 'none';
 
     if (isFile) {
-      return getFileSize(filePath).then(stats =>
-        console.log( `${fileName} - ${fileExt} - ${stats.size / 1024} kb` )
-      );
+
+      return getFileSize(filePath)
+        .then(stats =>
+          console.log(`${fileName} - ${fileExt} - ${stats.size > 1024
+            ? stats.size / 1024 + ' kb'
+            : stats.size + ' b'}`)
+        );
+
     }
 
   }));
