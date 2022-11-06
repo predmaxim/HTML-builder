1d2e42bd1db2255233df994dbcfae6aa8a26a5bd
Maxim Predybailo <predmaxim@ya.ru>
2022-11-06 06:46:24 +0500

    feat(02-write-file): implement write to file functionality

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+const process = require('process');
+
+const outputFile = path.join(__dirname, 'output.txt');
+const writeStream = fs.createWriteStream(outputFile);
+
+const hello = () => console.log('Hello! please, enter your text below:');
+const goodbye = () => {
+  console.log(`\nGoodbye!, your data in ${outputFile}`);
+  writeStream.end();
+  process.exit();
+};
+
+process.stdin.write('', hello);
+process.on('SIGINT', goodbye);
+
+process.stdin.on('data', (data) => data.toString() == 'exit\n' ? goodbye() : writeStream.write(data));
